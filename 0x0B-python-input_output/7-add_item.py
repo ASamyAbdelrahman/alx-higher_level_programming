cee7a8dabba0c00842b72ea223c8ef87e2b7ce33
ASamyAbdelrahman <abdelrahmansamy522@gmail.com>
2023-10-11 15:39:20 +0300

    Add Task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,27 +2,14 @@
 """Add all arguments to a Python list and save them to a file."""
 import sys
 
-total_size = 0
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-count = 0
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    for line in sys.stdin:
-        count += 1
-        split_line = line.split()
-        if len(split_line) > 2:
-            if split_line[-2] in status_codes:
-                status_codes[split_line[-2]] += 1
-            total_size += int(split_line[-1])
-        if count == 10:
-            print("File size: {}".format(total_size))
-            for key, value in sorted(status_codes.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
-            count = 0
-except KeyboardInterrupt:
-    print("File size: {}".format(total_size))
-    for key, value in sorted(status_codes.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
-    sys.exit(0)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
